badfa6c1e67630d85a668f101606b3f078a39087
Chris Hager <chris@linuxuser.at>
2021-05-10 20:20:44 +0200

    cleanup

--- pkg/ethtools/eth.go
+++ pkg/ethtools/eth.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"log"
 	"math/big"
+	"sync"
 
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
@@ -62,6 +63,115 @@ func NewResult() *AnalysisResult {
 	}
 }
 
+func (result *AnalysisResult) AddBlock(currentBlock *types.Block) {
+	if result.StartBlockTimestamp == 0 {
+		result.StartBlockTimestamp = currentBlock.Time()
+	}
+
+	result.EndBlockNumber = currentBlock.Number().Int64()
+	result.EndBlockTimestamp = currentBlock.Time()
+
+	result.NumBlocks += 1
+	result.NumTransactions += len(currentBlock.Transactions())
+
+	// Iterate over all transactions
+	for _, tx := range currentBlock.Transactions() {
+		// Count total value
+		result.ValueTotalWei = result.ValueTotalWei.Add(result.ValueTotalWei, tx.Value())
+
+		// Count number of transactions without value
+		if isBigIntZero(tx.Value()) {
+			result.NumTransactionsWithZeroValue += 1
+		}
+
+		// Count tx types
+		result.TxTypes[tx.Type()] += 1
+
+		// Count receiver stats
+		recAddr := "-"
+		if tx.To() != nil {
+			recAddr = tx.To().String()
+		}
+		toAddrInfo, exists := result.Addresses[recAddr]
+		if !exists {
+			toAddrInfo = NewAddressInfo(recAddr)
+			if tx.To() != nil { // only keep around if it has a receiver
+				result.Addresses[recAddr] = toAddrInfo
+			}
+		}
+
+		toAddrInfo.NumTxReceived += 1
+		toAddrInfo.ValueReceivedWei = new(big.Int).Add(toAddrInfo.ValueReceivedWei, tx.Value())
+		toAddrInfo.ValueReceivedEth = WeiToEth(toAddrInfo.ValueReceivedWei).Text('f', 2)
+
+		// Process FROM address (see https://goethereumbook.org/en/transaction-query/)
+		if msg, err := tx.AsMessage(types.NewEIP155Signer(tx.ChainId())); err == nil {
+			fromHex := msg.From().Hex()
+			// fmt.Println(fromHex)
+			fromAddrInfo, exists := result.Addresses[fromHex]
+			if !exists {
+				fromAddrInfo = NewAddressInfo(fromHex)
+				result.Addresses[fromHex] = fromAddrInfo
+			}
+
+			fromAddrInfo.NumTxSent += 1
+			fromAddrInfo.ValueSentWei = big.NewInt(0).Add(fromAddrInfo.ValueSentWei, tx.Value())
+			fromAddrInfo.ValueSentEth = WeiToEth(fromAddrInfo.ValueSentWei).Text('f', 2)
+		}
+
+		// Check for token transfer
+		data := tx.Data()
+		if len(data) > 0 {
+			result.NumTransactionsWithData += 1
+			toAddrInfo.NumTxWithData += 1
+
+			if len(data) > 4 {
+				methodId := hex.EncodeToString(data[:4])
+				// fmt.Println(methodId)
+				if methodId == "a9059cbb" || methodId == "23b872dd" {
+					result.NumTransactionsWithTokenTransfer += 1
+					toAddrInfo.NumTxTokenTransfer += 1
+
+					// Calculate and store the number of tokens transferred
+					var value string
+					switch methodId {
+					case "a9059cbb": // transfer
+						// targetAddr = hex.EncodeToString(data[4:36])
+						value = hex.EncodeToString(data[36:68])
+						toAddrInfo.NumTxTokenMethodTransfer += 1
+					case "23b872dd": // transferFrom
+						// targetAddr = hex.EncodeToString(data[36:68])
+						value = hex.EncodeToString(data[68:100])
+						toAddrInfo.NumTxTokenMethodTransferFrom += 1
+					}
+					valBigInt := new(big.Int)
+					valBigInt.SetString(value, 16)
+					toAddrInfo.TokensTransferred = new(big.Int).Add(toAddrInfo.TokensTransferred, valBigInt)
+
+					// // Debug helper
+					// errVal, _ := new(big.Int).SetString("1000000000000000000000000000", 10)
+					// if toAddrInfo.Address == "0x0D8775F648430679A709E98d2b0Cb6250d2887EF" {
+					// 	if valBigInt.Cmp(errVal) == 1 {
+					// 		fmt.Printf("BAT %s \t %s \t %v \t %s \t %s \n", tx.Hash(), value, valBigInt, valBigInt.String(), toAddrInfo.TokensTransferred.String())
+					// 	}
+					// }
+				}
+			}
+		}
+	}
+}
+
+// Queue for blocks to be added to DB
+var wg sync.WaitGroup
+
+func addBlockToDbWorker(db *sqlx.DB, jobChan <-chan *types.Block) {
+	defer wg.Done()
+
+	for block := range jobChan {
+		AddBlockToDatabase(db, block)
+	}
+}
+
 // Analyze blocks starting at specific block number, until a certain target timestamp
 func AnalyzeBlocks(client *ethclient.Client, startBlockNumber int64, endTimestamp int64, db *sqlx.DB) *AnalysisResult {
 	result := NewResult()
@@ -69,6 +179,12 @@ func AnalyzeBlocks(client *ethclient.Client, startBlockNumber int64, endTimestam
 
 	currentBlockNumber := big.NewInt(startBlockNumber)
 	numBlocksProcessed := 0
+
+	blocksForDbQueue := make(chan *types.Block, 100)
+	if db != nil {
+		wg.Add(1)
+		go addBlockToDbWorker(db, blocksForDbQueue)
+	}
 
 	for {
 		// fmt.Println(currentBlockNumber, "fetching...")
@@ -85,112 +201,24 @@ func AnalyzeBlocks(client *ethclient.Client, startBlockNumber int64, endTimestam
 		}
 
 		if db != nil {
-			AddBlockToDatabase(db, currentBlock)
+			blocksForDbQueue <- currentBlock
 		}
 
-		if result.StartBlockTimestamp == 0 {
-			result.StartBlockTimestamp = currentBlock.Time()
-		}
+		result.AddBlock(currentBlock)
 
-		result.EndBlockNumber = currentBlockNumber.Int64()
-		result.EndBlockTimestamp = currentBlock.Time()
-
-		result.NumBlocks += 1
-		result.NumTransactions += len(currentBlock.Transactions())
-
-		// Iterate over all transactions
-		for _, tx := range currentBlock.Transactions() {
-			// Count total value
-			result.ValueTotalWei = result.ValueTotalWei.Add(result.ValueTotalWei, tx.Value())
-
-			// Count number of transactions without value
-			if isBigIntZero(tx.Value()) {
-				result.NumTransactionsWithZeroValue += 1
-			}
-
-			// Count tx types
-			result.TxTypes[tx.Type()] += 1
-
-			// Count receiver stats
-			recAddr := "-"
-			if tx.To() != nil {
-				recAddr = tx.To().String()
-			}
-			toAddrInfo, exists := result.Addresses[recAddr]
-			if !exists {
-				toAddrInfo = NewAddressInfo(recAddr)
-				if tx.To() != nil { // only keep around if it has a receiver
-					result.Addresses[recAddr] = toAddrInfo
-				}
-			}
-
-			toAddrInfo.NumTxReceived += 1
-			toAddrInfo.ValueReceivedWei = new(big.Int).Add(toAddrInfo.ValueReceivedWei, tx.Value())
-			toAddrInfo.ValueReceivedEth = WeiToEth(toAddrInfo.ValueReceivedWei).Text('f', 2)
-
-			// Process FROM address (see https://goethereumbook.org/en/transaction-query/)
-			if msg, err := tx.AsMessage(types.NewEIP155Signer(tx.ChainId())); err == nil {
-				fromHex := msg.From().Hex()
-				// fmt.Println(fromHex)
-				fromAddrInfo, exists := result.Addresses[fromHex]
-				if !exists {
-					fromAddrInfo = NewAddressInfo(fromHex)
-					result.Addresses[fromHex] = fromAddrInfo
-				}
-
-				fromAddrInfo.NumTxSent += 1
-				fromAddrInfo.ValueSentWei = big.NewInt(0).Add(fromAddrInfo.ValueSentWei, tx.Value())
-				fromAddrInfo.ValueSentEth = WeiToEth(fromAddrInfo.ValueSentWei).Text('f', 2)
-			}
-
-			// Check for token transfer
-			data := tx.Data()
-			if len(data) > 0 {
-				result.NumTransactionsWithData += 1
-				toAddrInfo.NumTxWithData += 1
-
-				if len(data) > 4 {
-					methodId := hex.EncodeToString(data[:4])
-					// fmt.Println(methodId)
-					if methodId == "a9059cbb" || methodId == "23b872dd" {
-						result.NumTransactionsWithTokenTransfer += 1
-						toAddrInfo.NumTxTokenTransfer += 1
-
-						// Calculate and store the number of tokens transferred
-						var value string
-						switch methodId {
-						case "a9059cbb": // transfer
-							// targetAddr = hex.EncodeToString(data[4:36])
-							value = hex.EncodeToString(data[36:68])
-							toAddrInfo.NumTxTokenMethodTransfer += 1
-						case "23b872dd": // transferFrom
-							// targetAddr = hex.EncodeToString(data[36:68])
-							value = hex.EncodeToString(data[68:100])
-							toAddrInfo.NumTxTokenMethodTransferFrom += 1
-						}
-						valBigInt := new(big.Int)
-						valBigInt.SetString(value, 16)
-						toAddrInfo.TokensTransferred = new(big.Int).Add(toAddrInfo.TokensTransferred, valBigInt)
-
-						// // Debug helper
-						// errVal, _ := new(big.Int).SetString("1000000000000000000000000000", 10)
-						// if toAddrInfo.Address == "0x0D8775F648430679A709E98d2b0Cb6250d2887EF" {
-						// 	if valBigInt.Cmp(errVal) == 1 {
-						// 		fmt.Printf("BAT %s \t %s \t %v \t %s \t %s \n", tx.Hash(), value, valBigInt, valBigInt.String(), toAddrInfo.TokensTransferred.String())
-						// 	}
-						// }
-					}
-				}
-			}
-		}
-
-		currentBlockNumber.Add(currentBlockNumber, One)
+		// Increase current block number and number of blocks processed
 		numBlocksProcessed += 1
+		currentBlockNumber.Add(currentBlockNumber, One)
+
+		// negative endTimestamp is a helper to process only a certain number of blocks. If reached, then end now
 		if endTimestamp < 0 && numBlocksProcessed*-1 == int(endTimestamp) {
 			break
 		}
 	}
 
+	// Close DB-add-block channel and wait for all to be saved
+	close(blocksForDbQueue)
+	wg.Wait()
 	return result
 }
 
