0c04640b05f561b95eb75c0dfad9892560c69a6a
Chris Hager <chris@linuxuser.at>
2021-05-31 10:27:15 +0200

    simpler templatedata getters

--- templates/templatedata.go
+++ templates/templatedata.go
@@ -11,65 +11,51 @@ type TemplateData struct {
 	AddressStats *[]ethstats.AnalysisAddressStatsEntryWithAddress
 }
 
-func (tv *TemplateData) GetTopErc20Transfer(start int, maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
+func (tv *TemplateData) GetTopStats(start int, maxEntries int, sortMethod func(i int, j int) bool, checkMethod func(a ethstats.AnalysisAddressStatsEntryWithAddress) bool) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
 	if start > len(*tv.AddressStats) {
 		return nil
 	}
 
-	sort.SliceStable(*tv.AddressStats, func(i, j int) bool {
-		return (*tv.AddressStats)[i].NumTxErc20Transfer > (*tv.AddressStats)[j].NumTxErc20Transfer
-	})
+	sort.SliceStable(*tv.AddressStats, sortMethod)
 
 	res := make([]ethstats.AnalysisAddressStatsEntryWithAddress, 0, maxEntries)
 	for i := start; i < start+maxEntries && i < len(*tv.AddressStats); i++ {
-		if (*tv.AddressStats)[i].NumTxErc20Transfer > 0 {
+		entry := (*tv.AddressStats)[i]
+		if checkMethod(entry) {
 			res = append(res, (*tv.AddressStats)[i])
 		}
 	}
 	return &res
 }
 
-func (tv *TemplateData) GetTopErc721Transfer(maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
-	sort.SliceStable(*tv.AddressStats, func(i, j int) bool {
+func (tv *TemplateData) GetTopErc20Transfer(start int, maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
+	sortMethod := func(i, j int) bool {
+		return (*tv.AddressStats)[i].NumTxErc20Transfer > (*tv.AddressStats)[j].NumTxErc20Transfer
+	}
+	checkMethod := func(a ethstats.AnalysisAddressStatsEntryWithAddress) bool { return a.NumTxErc20Transfer > 0 }
+	return tv.GetTopStats(start, maxEntries, sortMethod, checkMethod)
+}
+
+func (tv *TemplateData) GetTopErc721Transfer(start int, maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
+	sortMethod := func(i, j int) bool {
 		return (*tv.AddressStats)[i].NumTxErc721Transfer > (*tv.AddressStats)[j].NumTxErc721Transfer
-	})
-
-	// Add to result
-	res := make([]ethstats.AnalysisAddressStatsEntryWithAddress, 0)
-	for i := 0; i < maxEntries && i < len(*tv.AddressStats); i++ {
-		if (*tv.AddressStats)[i].NumTxErc721Transfer > 0 {
-			res = append(res, (*tv.AddressStats)[i])
-		}
 	}
-	return &res
+	checkMethod := func(a ethstats.AnalysisAddressStatsEntryWithAddress) bool { return a.NumTxErc721Transfer > 0 }
+	return tv.GetTopStats(start, maxEntries, sortMethod, checkMethod)
 }
 
-func (tv *TemplateData) GetTopFailedTxReceivers(maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
-	sort.SliceStable(*tv.AddressStats, func(i, j int) bool {
+func (tv *TemplateData) GetTopFailedTxReceivers(start int, maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
+	sortMethod := func(i, j int) bool {
 		return (*tv.AddressStats)[i].NumTxReceivedFailed > (*tv.AddressStats)[j].NumTxReceivedFailed
-	})
-
-	// Add to result
-	res := make([]ethstats.AnalysisAddressStatsEntryWithAddress, 0)
-	for i := 0; i < maxEntries && i < len(*tv.AddressStats); i++ {
-		if (*tv.AddressStats)[i].NumTxReceivedFailed > 0 {
-			res = append(res, (*tv.AddressStats)[i])
-		}
 	}
-	return &res
+	checkMethod := func(a ethstats.AnalysisAddressStatsEntryWithAddress) bool { return a.NumTxReceivedFailed > 0 }
+	return tv.GetTopStats(start, maxEntries, sortMethod, checkMethod)
 }
 
-func (tv *TemplateData) GetTopFailedTxSender(maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
-	sort.SliceStable(*tv.AddressStats, func(i, j int) bool {
+func (tv *TemplateData) GetTopFailedTxSender(start int, maxEntries int) *[]ethstats.AnalysisAddressStatsEntryWithAddress {
+	sortMethod := func(i, j int) bool {
 		return (*tv.AddressStats)[i].NumTxSentFailed > (*tv.AddressStats)[j].NumTxSentFailed
-	})
-
-	// Add to result
-	res := make([]ethstats.AnalysisAddressStatsEntryWithAddress, 0)
-	for i := 0; i < maxEntries && i < len(*tv.AddressStats); i++ {
-		if (*tv.AddressStats)[i].NumTxSentFailed > 0 {
-			res = append(res, (*tv.AddressStats)[i])
-		}
 	}
-	return &res
+	checkMethod := func(a ethstats.AnalysisAddressStatsEntryWithAddress) bool { return a.NumTxSentFailed > 0 }
+	return tv.GetTopStats(start, maxEntries, sortMethod, checkMethod)
 }
